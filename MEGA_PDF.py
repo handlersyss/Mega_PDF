25a7ff095073256fcee2f183e830a26ebd0e891e
handlersyss <edsontaylor@outlook.com.br>
2025-01-29 11:08:01 -0300

    Corrigindo subprocess

--- MEGA_PDF.py
+++ MEGA_PDF.py
@@ -90,6 +90,7 @@ def word_para_pdf(files):
             arquivos = verificar_e_obter_caminhos(files)
             for abs_path in arquivos:
                 try:
+                    output_dir = os.path.dirname(abs_path)
                     subprocess.run(['/usr/bin/libreoffice', '--headless', '--convert-to', 'pdf', abs_path], check=True)           
                     messagebox.showinfo("Sucesso", f"Arquivo Word '{abs_path}' convertido para PDF com sucesso.")
                 except subprocess.CalledProcessError as err:
